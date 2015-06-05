5f0e46f2e717cc5550862530bfe1b0908deb5003
Chris Detsch <cd62936@email.appstate.edu>
2015-06-05 15:17:26 -0400

    Added bootstrap to Assignments By Type report and changed the View file so that it no longer gives an exception from the constant method.

--- class/report/AssignmentsByType/AssignmentsByTypeHtmlView.php
+++ class/report/AssignmentsByType/AssignmentsByTypeHtmlView.php
@@ -1,37 +1,36 @@
 <?php
 
 class AssignmentsByTypeHtmlView extends ReportHtmlView {
-    
+
     protected function render(){
         parent::render();
-        
+
         $this->tpl['TERM'] = Term::toString($this->report->getTerm());
-        
+
         $rows = array();
-        
+
         $totalAssignments = 0;
-        
-        foreach($this->report->getTypeCounts() as $reason=>$count){
+
+        foreach($this->report->getTypeCounts() as $reasons){
             $row = array();
-            
-            $name = constant($reason);
+            $name = constant($reasons['reason']);
             if(isset($name)){
                 $row['REASON'] = $name;
             }else{
                 $row['REASON'] = $reason;
-            } 
-            
-            $row['COUNT'] = $count;
-            
+            }
+
+            $row['COUNT'] = $reasons['count'];
+
             $rows[] = $row;
-            
-            $totalAssignments += $count;
+
+            $totalAssignments += $reasons['count'];
         }
 
         $this->tpl['TABLE_ROWS'] = $rows;
-        
+
         $this->tpl['TOTAL_ASSIGNMENTS'] = $totalAssignments;
-        
+
         return PHPWS_Template::process($this->tpl, 'hms', 'admin/reports/AssignmentsByType.tpl');
     }
 }
--- templates/admin/reports/AssignmentsByType.tpl
+++ templates/admin/reports/AssignmentsByType.tpl
@@ -1,23 +1,28 @@
-<h1>{NAME} - {TERM}</h1>
+<h2>{NAME} <small>{TERM}</small></h2>
 
-Executed on: {EXEC_DATE} by {EXEC_USER}
-<br />
+<div class="col-md-6">
+  <p>
+    Executed on: {EXEC_DATE} by {EXEC_USER}
+  </p>
+</div>
 
-<table>
-  <tr>
-    <th>Assignment Reason</th>
-    <th># Assignments</th>
-  </tr>
+<div class="col-md-8">
+  <table class="table table-striped table-hover">
+    <tr>
+      <th>Assignment Reason</th>
+      <th># Assignments</th>
+    </tr>
 
-  <!-- BEGIN TABLE_ROWS -->
-  <tr>
-    <td>{REASON}</td>
-    <td>{COUNT}</td>
-  </tr>
-  <!-- END TABLE_ROWS -->
-  
-  <tr>
-    <td><strong>Total:</strong></td>
-    <td><strong>{TOTAL_ASSIGNMENTS}</strong></td>
-  </tr>
-</table>
+    <!-- BEGIN TABLE_ROWS -->
+    <tr>
+      <td>{REASON}</td>
+      <td>{COUNT}</td>
+    </tr>
+    <!-- END TABLE_ROWS -->
+
+    <tr>
+      <td><strong>Total:</strong></td>
+      <td><strong>{TOTAL_ASSIGNMENTS}</strong></td>
+    </tr>
+  </table>
+</div>
